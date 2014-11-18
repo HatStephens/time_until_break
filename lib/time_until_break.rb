205a806d9fada4a43f0dc5b15ec1a1feba548f44
Stephen Giles <s.giles@hotmail.co.uk>
2014-11-18 10:02:30 +0000

    Gem appears to be working - 5 tests passing. Made 1 of those 'pending' as based upon a changing result

--- lib/time_until_break.rb
+++ lib/time_until_break.rb
@@ -1,14 +1,34 @@
 require_relative "time_until_break/version"
+require 'time'
 
 class TimeUntilBreak
 
   attr_reader :break_time
 
-  def initialize (break_time="16:45:00")
-    @break_time = break_time
+  def initialize (break_time="16:45")
+    @break_time = Time.parse(break_time)
+  end
+
+  def message
+    has_break_been? ? missed_break : time_until_break
+  end
+
+  def time_until_break
+    seconds = @break_time.to_i - current_time.to_i
+    hours = (seconds/(60*60))
+    minutes = ((seconds - (hours*60*60))/60)
+    %x(say "#{hours} hours, #{minutes} minutes")
+  end
+
+  def has_break_been?
+    current_time > @break_time
+  end
+
+  def missed_break
+    %x(say "Sorry you missed the Mandatory Break.")
   end
 
   def current_time
-    Time.now.strftime("%H:%M:%S")
+    Time.now
   end
 end
--- spec/time_until_break_spec.rb
+++ spec/time_until_break_spec.rb
@@ -4,17 +4,25 @@ describe TimeUntilBreak do
 
   let(:time){TimeUntilBreak.new}
 
-  it 'knows the current time, exact to the second' do
-    expect(time.current_time).to eq Time.now.strftime("%H:%M:%S")
+  it 'knows the current time, exact to the minute' do
+    expect(time.current_time.strftime("%H:%M")).to eq Time.now.strftime("%H:%M")
   end
 
   it 'knows what time the Mandatory Break is' do
-    expect(time.break_time).to eq "16:45:00"
+    expect(time.break_time.strftime("%H:%M")).to eq "16:45"
   end
 
   it 'can have a new break time set' do
     time_two = TimeUntilBreak.new("14:30:00")
-    expect(time_two.break_time).to eq "14:30:00"
+    expect(time_two.break_time.strftime("%H:%M:%S")).to eq "14:30:00"
+  end
+
+  it 'knows if the break has been yet' do
+    expect(time.has_break_been?).to eq false
+  end
+
+  xit 'can calculate how long until the next break in hours and minutes' do
+    expect(time.message).to eq "6 hours, 50 minutes"
   end
 
 end
